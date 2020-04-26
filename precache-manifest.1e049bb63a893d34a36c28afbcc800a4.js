c0cd774fc45137499260cca57fba20d9d2ba2de5
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2020-04-26 12:27:37 -0400

    Updates

---
+++ precache-manifest.1e049bb63a893d34a36c28afbcc800a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d581d0830a3e05a2050d69e28fb148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8e31947ad68d6f1bcd6",
+    "url": "/static/css/main.e11586f6.chunk.css"
+  },
+  {
+    "revision": "1f6695b3a700060e2ca0",
+    "url": "/static/js/2.a59bb7ef.chunk.js"
+  },
+  {
+    "revision": "3e03be6ff809735ea235b8c97bfeca80",
+    "url": "/static/js/2.a59bb7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e31947ad68d6f1bcd6",
+    "url": "/static/js/main.bcdee8fb.chunk.js"
+  },
+  {
+    "revision": "68be8c75e3bf81553fd7c35052453a64",
+    "url": "/static/js/main.bcdee8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459301d563c1c2151454",
+    "url": "/static/js/runtime-main.3050de33.js"
+  },
+  {
+    "revision": "c29888b59105009fed30d90c8be1621a",
+    "url": "/static/media/click.c29888b5.svg"
+  },
+  {
+    "revision": "9af81ed86a2a6802d977e4b472ee9c02",
+    "url": "/static/media/drag.9af81ed8.svg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
